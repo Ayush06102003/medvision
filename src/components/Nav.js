87abaac30820d1c2e1c8946552bc08a78ae9d941
Madhura <113138705+Madhuraaa06@users.noreply.github.com>
2024-06-20 20:52:49 +0530

    change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -92,8 +92,8 @@ function Nav({ mode }) {
             >
               <img
                 src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
-                }
+                  "/assets/logo.png"
+                  }
                 style={logoStyle}
                 alt="logo of sitemark"
               />
